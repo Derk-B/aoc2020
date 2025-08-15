8e63827eac17ae4b0a6039bd0f8b6114198bcab0
Derk-B <74244634+Derk-B@users.noreply.github.com>
2025-08-16 00:46:14 +0200

    Added day selector

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,33 @@ mod day1;
 mod day2;
 mod shared;
 
+use std::env;
+
 fn main() {
-    day1::solver::solve();
-    day2::solver::solve();
+    let solvers: Vec<&dyn Fn()> = vec![&day1::solver::solve, &day2::solver::solve];
+
+    let selected_day = env::args().nth(1);
+
+    if let Some(day_str) = selected_day {
+        let day = match day_str.parse::<usize>() {
+            Ok(d) => d,
+            Err(_) => panic!(
+                "Selected day argument must be a positive integer, not '{}'.",
+                day_str
+            ),
+        };
+
+        if day > solvers.len() {
+            panic!(
+                "Selected day out of bounds, must be between 1 and {}.",
+                solvers.len()
+            )
+        }
+
+        solvers.iter().nth(day - 1).unwrap()();
+    } else {
+        for solver in solvers {
+            solver();
+        }
+    }
 }
