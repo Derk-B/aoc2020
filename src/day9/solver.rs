51d9d64a1511c9991bc68908ba84ac618aa760c9
Derk-B <74244634+Derk-B@users.noreply.github.com>
2025-08-19 21:06:59 +0200

    Day 9

--- src/day9/solver.rs
+++ src/day9/solver.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 use crate::shared::file_reader;
 
 fn range_contains_sum(range: &[u64], sum: &u64) -> bool {
@@ -46,16 +48,14 @@ pub fn solve() {
     let mut range: Vec<u64> = Vec::new();
     while idx < numbers.len() {
         let mut i: usize = idx;
-        while i + idx < numbers.len() {
-            if sum + numbers[idx + i] > found_value {
-                // print!("{} > {}", sum + numbers[idx + i]);
+        while i < numbers.len() {
+            if sum + numbers[i] > found_value {
                 range = Vec::new();
                 sum = 0;
                 break;
             }
-            println!("Next");
-            if sum + numbers[idx + i] == found_value {
-                range.push(numbers[i + idx]);
+            if sum + numbers[i] == found_value && range.len() >= 1 {
+                range.push(numbers[i]);
                 let mut m1: u64 = range[0];
                 let mut m2: u64 = range[0];
                 for n in &range {
@@ -66,21 +66,15 @@ pub fn solve() {
                         m2 = *n;
                     }
                 }
-                let mres = m1 + m2;
-                println!("Day 9 - part 2: {} {} {}", m1, m2, mres);
-                let total = range.iter().fold(0, |acc, n| acc + n);
-                println!("Total: {}", total + numbers[idx + i]);
+                let res = m1 + m2;
+                println!("Day 9 - part 2: {}", res);
                 break;
             }
 
-            range.push(numbers[idx + i]);
-            sum += numbers[idx + i];
+            range.push(numbers[i]);
+            sum += numbers[i];
             i += 1;
         }
         idx += 1;
-        // println!("IDX: {}", idx);
     }
 }
-
-// 1187622051
-// 1309761972
